8f2123bd396ce873fa9d63aa8ae73274dfe8c762
Gustavo Tello Beltran <gustel318@gmail.com>
2019-11-28 12:34:31 +0100

    examenldap

--- hostpam19:aware/pam_mates.py
+++ hostpam19:aware/pam_mates.py
@@ -1,22 +1,27 @@
+# -*-coding: utf-8-*-
 # pam_mates.py
-# Validar l’usuari realitzant una pregunta de matemàtiques
-# isx43577298 curs 2019-20
-# 28-11-19
-def pam_sm_authenticat(pamh, flags, argv):
-    print "Quant fan 3*2?"
-    resposta=raw_input()
-if int(resposta) == 6:
+# Validar l'usuari realitzant una pregunta de matematiques
+
+def pam_sm_authenticate (pamh, flags, argv):
+  print "Quant fan 2*5?"
+  resposta=raw_input()
+  if int(resposta) == 10:
     return pamh.PAM_SUCCESS
-else:
+  else:
     return pamh.PAM_AUTHTOK_ERR
 
 def pam_sm_setcred (pamh, flags, argv):
-    return pamh.PAM_SUCCESS
+  return pamh.PAM_SUCCESS
+
 def pam_sm_acct_mgmt (pamh, flags, argv):
-    return pamh.PAM_SUCCESS
-def pam_sm_open_session ( pamh, flags, argv):
-    return pamh.PAM_SUCCESS
+  return pamh.PAM_SUCCESS
+
+def pam_sm_open_session (pamh, flags, argv):
+  return pamh.PAM_SUCCESS
+
 def pam_sm_close_session (pamh, flags, argv):
-    return pamh.PAM_SUCCESS
+  return pamh.PAM_SUCCESS
+
 def pam_sm_chauthtok (pamh, flags, argv):
-    return pamh.PAM_SUCCESS
+  return pamh.PAM_SUCCESS
+
